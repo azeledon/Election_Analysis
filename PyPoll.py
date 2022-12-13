590bcaf4f63adc53c1dd8a845129119538778bfd
azeledon <valeriazrod3@gmail.com>
2022-12-13 13:45:53 -0700

    update Pypoll file

--- PyPoll.py
+++ PyPoll.py
@@ -6,20 +6,18 @@
 # The winner of the election based on popular vote
 # Open the election results and read the file
 
+# Add our dependencies.
 import csv
 import os
-# Assign a variable for the file to load and the path.
+# Assign a variable to load a file from a path.
 file_to_load = os.path.join("Resources", "election_results.csv")
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
+    file_reader = csv.reader(election_data)
 
-    # Print the file object.
-     print(election_data)
-
-
-
-
-
-
-
-
+    # Read and print the header row.
+    headers = next(file_reader)
+    print(headers)
