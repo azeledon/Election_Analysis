da1485e338e632a2ce20d50dd6a2aa275ed6a655
azeledon <valeriazrod3@gmail.com>
2022-12-13 11:25:37 -0700

    Add analysis folder

--- PyPoll.py
+++ PyPoll.py
@@ -4,24 +4,22 @@
 # Total number of votes each candidate received
 # Percentage of votes each candidate won
 # The winner of the election based on popular vote
+# Open the election results and read the file
 
-# Add our dependencies.
 import csv
 import os
 # Assign a variable for the file to load and the path.
-file_to_load='Resources/election_results.csv'
-# Assign a variable to save the file to a path.
-file_to_save = os.path.join("Analysis","election_analysis.txt")
-
+file_to_load = os.path.join("Resources", "election_results.csv")
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
 
-# To do : read and analyze the data here.
-    file_reader = csv.reader(election_data)
+    # Print the file object.
+     print(election_data)
 
-    # Read and Print the hearder row.
-    headers = next(file_reader)
-    print(headers)
+
+
+
+
 
 
 
