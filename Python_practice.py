588de6e5f331bfcf8a714a11bb41a201ef9af0b9
azeledon <valeriazrod3@gmail.com>
2022-12-12 20:38:46 -0700

    Update files

--- Python_practice.py
+++ Python_practice.py
@@ -1,1 +1,8 @@
-print("Hello World")
+counties_dict = {"Arapahoe": 422829, "Denver":463353, "Jefferson": 432438}
+for county, voters in counties_dict.items():
+    print(county + " county has " + str(voters) + " registered voters.")
+
+
+
+
+
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,7 +1,3 @@
-counties = ["Arapahoe","Denver","Jefferson"]
-if counties[1] == 'Denver':
-    print(counties[1])
-
 temperature = int(input("What is the temperature outside? "))
 if temperature > 80:
     print("Turn on the AC.")
