20f316135042883dab4d7bdfbe900dbd6b58fff1
azeledon <valeriazrod3@gmail.com>
2022-12-14 11:23:14 -0700

    Update folder

--- New_Election_Analysis.py
+++
@@ -1,9 +0,0 @@
-counties = ["Arapahoe","Denver","Jefferson"]
-if counties[1] == 'Denver':
-    print(counties[1])
-
-temperature = int(input("What is the temperature outside? "))
-if temperature > 80:
-    print("Turn on the AC.")
-else:
-    print("Open the windows.")    
--- Python_practice.py
+++
@@ -1,8 +0,0 @@
-counties_dict = {"Arapahoe": 422829, "Denver":463353, "Jefferson": 432438}
-for county, voters in counties_dict.items():
-    print(county + " county has " + str(voters) + " registered voters.")
-
-
-
-
-
